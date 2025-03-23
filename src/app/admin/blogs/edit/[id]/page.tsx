5de51e76e72e0730f893bd697a776a56f4ef8f23
manisense <imanishmeenaa@gmail.com>
2025-03-23 23:50:30 +0530

    blog text in admin

--- src/app/admin/blogs/edit/[id]/page.tsx
+++ src/app/admin/blogs/edit/[id]/page.tsx
@@ -210,10 +210,10 @@ export default function EditBlogPost({ params }: { params: Promise<{ id: string 
   }
 
   return (
-    <div className="p-6">
+    <div className="p-6 text-gray-900">
       <div className="flex items-center justify-between mb-6">
         <div className="flex items-center">
-          <Link href="/admin/blogs" className="text-gray-500 hover:text-gray-700 mr-4">
+          <Link href="/admin/blogs" className=" hover:text-gray-700 mr-4">
             <FaArrowLeft />
           </Link>
           <h1 className="text-2xl font-bold">Edit Blog Post</h1>
